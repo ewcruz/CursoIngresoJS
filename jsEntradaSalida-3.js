755d5349a49fb042fdccdd695807ee2f72648221
ewcruz <47437168+ewcruz@users.noreply.github.com>
2019-02-07 21:56:17 -0300

    Primer Commit
    Ejercicios Clase 1 al 4

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	//elNombre.value="Edward Wayne Cruz";
+	var nombre;
+	nombre=prompt("Please enter your name", "Edward Wayne Cruz");
+	alert("Edward Wayne Cruz");
+}
+
+
+
+
