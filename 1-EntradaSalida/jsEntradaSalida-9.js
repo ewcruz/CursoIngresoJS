e73ad8f5d617c8a4dedbaeae30a6bf259e62e4a2
alumno <alumno>
2019-02-08 22:29:08 -0300

    ejercicio 9 defectuoso en clase

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,21 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var salario;
+	var aumento;
+	var salariofinal;
+
+	primerNumero=sueldo.value;
+	segundoNumero=resultado.value;
+	aumento=salario*10/100 
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	salariofinal=salario+aumento;
+
+	resultado.value=salariofinal;
+
 
 	
 }
