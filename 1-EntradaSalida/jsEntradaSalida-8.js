8f537490934f1e6b141ac8fdde9405a026be420a
alumno <alumno>
2019-02-08 21:49:19 -0300

    ejercicio 8 resuelto en clase

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,19 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var primerNumero;
+	var segundoNumero;
+	var resto;
+
+	primerNumero=numeroDividendo.value;
+	segundoNumero=numeroDivisor.value;
+
+	primerNumero=parseInt(primerNumero);
+	segundoNumero=parseInt(segundoNumero);
+
+	resto=primerNumero%segundoNumero
+
+	alert(" el resto de la division es " + resto)
+
+
 }
