31213544e832818e7946e80c7cad1d19dc304450
alumno <alumno>
2019-02-14 19:15:56 -0300

    ejercicio uno.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,11 @@
 
 function mostrar()
 {
+	var pesoLila;
+	var pesoGorutta;
 
+	pesoLila=10;
+	pesoGorutta=12;
+	
+	alert (tenes dos mascotas Lila y Gorutta, que pesan 10 y 12 kilos, la suma de los kilos es pesoLila+pesoGorutta );
 }
