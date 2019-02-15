63191e71b9d84195690e39ab9485f3e9b4cdd78a
alumno <alumno>
2019-02-15 20:26:44 -0300

    switch ejercicio 1

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,8 +2,30 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
+var mensaje
 
+switch(mesDelAño)
+{
+  	case "Enero":
+  	mensaje="Que comiences bien el año!!";
+    break;
+  
+  	case "Marzo":
+  	mensaje="A Clases!!";
+    break;
+  
 
+  	case "Julio":
+  	mensaje="Se vienen las vacaciones!!";
+  	break;
+
+  	case "Diciembre":
+  	mensaje="Felices Fiestas!!";
+  	break;
+    
+}
+
+alert(mensaje);
 
 
 }//FIN DE LA FUNCIÓN
