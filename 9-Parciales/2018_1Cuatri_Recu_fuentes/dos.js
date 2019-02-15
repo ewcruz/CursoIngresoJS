28f374da6bce2fcffc527700158bac13eaa51def
alumno <alumno>
2019-02-14 21:23:25 -0300

    ejercicio 2 sin funcionar.js

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
-  
+	var compra;
+	var descuento;
+	var preciodescuento;
+	var iva;
+	var preciofinal;
+
+	compra=prompt("Importe");
+	descuento=compra*10/100;
+    preciodescuento=compra-descuento;
+	iva=preciodescuento*21/100;
+
+	compra=parseInt(compra);
+	descuento=parseInt(descuento);
+	preciodescuento=parseInt(preciodescuento);
+
+  alert("Tu compra es de " +compra+ ", tenes un descuento del 10%, queda en $" +descuento+ ", mas el IVA es " )
 }
