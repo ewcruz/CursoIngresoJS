3b328d7a6d0af2d0c40cad9611bf31a3a48e9f2e
alumno <alumno>
2019-02-15 21:03:23 -0300

    switch ejercicio 3

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -2,9 +2,20 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+var mensaje
 //alert (mesDelAño);
-	
+switch(mesDelAño)
+{
+  	case "Febrero":
+  		mensaje="Este mes no tiene mas de 29 dias";
+    	break;
+  
+  	default:
+  		mensaje="Este mes tiene 30 o mas dias";
+    
+}	
+
+alert(mensaje)
 	
 
 
