9ba73b4dea3f3f2caebf63067b5175b98771cafd
ewcruz <ewcruz@yahoo.com>
2019-02-21 21:19:05 -0300

    ejercicio 5 switch

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
+	var precioHabitacion;
+	var mensaje;
 
-}
+	precioHabitacion=prompt("Indique el monto a cobrar ","");
+	precioHabitacion=parseInt(precioHabitacion);
+
+	switch(precioHabitacion)
+	{
+		case "Visa":
+		mensaje=precioHabitacion*0.9;
+		break;
+
+		case "Paypal"
+		mensaje=precioHabitacion*0.85;
+		break;
+
+		case "mercadoPago"
+		mensaje=precioHabitacion*0.9;
+		break;
+
+		case "todoIncluido"
+		mensaje= 
+
+	}
