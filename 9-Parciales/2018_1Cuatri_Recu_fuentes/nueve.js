c1aefb44a7b7c46c2e5fc5acea439ede8e0f931b
alumno <alumno>
2019-02-28 22:08:22 -0300

    ejercicio de Recuperatorio 9 incompleto

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+// enunciado: tenemos una serie de celulares, pedimos marca, pedimos tamaño (numero), pedimos precio;
+//1)mayor precio y marca;
+//2)
+  	var marcaCelular;
+  	var tamañoCelular;
+  	var precioCelular;
 
+  	marcaCelular=prompt("Ingrese la marca aquí");
+  	tamañoCelular=prompt("Ingrese el peso aqui");
+  	precioCelular=prompt("Ingrese el precio aqui");
+
+  	 
 }
